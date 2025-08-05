64253750dbee6d7f477485b4e0aebc30dec87e23
sdkparkforbi <sdkparkforbi@gmail.com>
2025-08-05 21:31:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,6 @@ st.title("D-ID AI Agent")
 
 html_code = """
 <div id="did-agent-container" style="width: 100%; height: 600px;"></div>
-
-# <script type="module"
-#       src="https://agent.d-id.com/v2/index.js"
-#       data-mode="full"
-#       data-client-key="Z29vZ2xlLW9hdXRoMnwxMTI3NjQ3MzA0NTM3NjA0MTgyMTI6d01EN0x6bFFFMmlZSk9nUHNacXll"
-#       data-agent-id="v2_agt_80jV_9EA"
-#       data-name="did-agent"
-#       data-monitor="true"
-#       data-target-id="did-agent-container">
-# </script>
-
 <script type="module"
       src="https://agent.d-id.com/v2/index.js"
       data-mode="fabio"
@@ -31,4 +20,5 @@ html_code = """
 """
 
 components.html(html_code, height=650)
+
 
