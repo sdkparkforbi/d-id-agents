84a4bb8e328730f12e888d6815e17001007339d7
sdkparkforbi <sdkparkforbi@gmail.com>
2025-08-05 21:33:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ html_code = """
 <div id="did-agent-container" style="width: 100%; height: 600px;"></div>
 <script type="module"
       src="https://agent.d-id.com/v2/index.js"
-      data-mode="fabio"
+      data-mode="full"
       data-client-key="Z29vZ2xlLW9hdXRoMnwxMTI3NjQ3MzA0NTM3NjA0MTgyMTI6d01EN0x6bFFFMmlZSk9nUHNacXll"
       data-agent-id="v2_agt_Iog7Y5Ky"
       data-name="did-agent"
       data-monitor="true"
-      data-orientation="horizontal"
-      data-position="right">
+      data-target-id="did-agent-container">
 </script>
 
 """
 
 components.html(html_code, height=650)
+
 
 
