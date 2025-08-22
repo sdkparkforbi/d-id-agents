f8fcb59876715b76898a0c68111e838dd6278b46
sdkparkforbi <sdkparkforbi@gmail.com>
2025-08-22 09:13:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
+# import streamlit as st
+# import streamlit.components.v1 as components
+
+# st.title("D-ID AI Agent")
+
+# html_code = """
+# <div id="did-agent-container" style="width: 100%; height: 600px;"></div>
+# <script type="module"
+#       src="https://agent.d-id.com/v2/index.js"
+#       data-mode="full"
+#       data-client-key="Z29vZ2xlLW9hdXRoMnwxMTI3NjQ3MzA0NTM3NjA0MTgyMTI6d01EN0x6bFFFMmlZSk9nUHNacXll"
+#       data-agent-id="v2_agt_Iog7Y5Ky"
+#       data-name="did-agent"
+#       data-monitor="true"
+#       data-target-id="did-agent-container">
+# </script>
+# """
+
+# components.html(html_code, height=650)
+
 import streamlit as st
 import streamlit.components.v1 as components
 
@@ -9,16 +29,15 @@ html_code = """
       src="https://agent.d-id.com/v2/index.js"
       data-mode="full"
       data-client-key="Z29vZ2xlLW9hdXRoMnwxMTI3NjQ3MzA0NTM3NjA0MTgyMTI6d01EN0x6bFFFMmlZSk9nUHNacXll"
-      data-agent-id="v2_agt_Iog7Y5Ky"
+      data-agent-id="v2_agt_80jV_9EA"
       data-name="did-agent"
       data-monitor="true"
       data-target-id="did-agent-container">
 </script>
-
 """
 
 components.html(html_code, height=650)
-
+ 
 
 # import streamlit as st
 # import streamlit.components.v1 as components
@@ -73,5 +92,6 @@ components.html(html_code, height=650)
 # surprised - 놀라는 표정
 # happy - 기쁜 표정
 # sad - 슬픈 표정
+
 
 
