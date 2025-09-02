b66b7e6bcaefa333e8244a2d2dfe1fd4eb5e3a0d
sdkparkforbi <sdkparkforbi@gmail.com>
2025-09-03 08:42:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,26 +18,6 @@
 
 # components.html(html_code, height=650)
 
-# import streamlit as st
-# import streamlit.components.v1 as components
-
-# st.title("D-ID AI Agent")
-
-# html_code = """
-# <div id="did-agent-container" style="width: 80%; height: 600px;"></div>
-# <script type="module"
-#       src="https://agent.d-id.com/v2/index.js"
-#       data-mode="full"
-#       data-client-key="Z29vZ2xlLW9hdXRoMnwxMTI3NjQ3MzA0NTM3NjA0MTgyMTI6d01EN0x6bFFFMmlZSk9nUHNacXll"
-#       data-agent-id="v2_agt_80jV_9EA"
-#       data-name="did-agent"
-#       data-monitor="true"
-#       data-target-id="did-agent-container">
-# </script>
-# """
-
-# components.html(html_code, height=650)
-
 import streamlit as st
 import streamlit.components.v1 as components
 
@@ -47,18 +27,38 @@ html_code = """
 <div id="did-agent-container" style="width: 80%; height: 600px;"></div>
 <script type="module"
       src="https://agent.d-id.com/v2/index.js"
-      data-mode="fabio"
+      data-mode="full"
       data-client-key="Z29vZ2xlLW9hdXRoMnwxMTI3NjQ3MzA0NTM3NjA0MTgyMTI6d01EN0x6bFFFMmlZSk9nUHNacXll"
       data-agent-id="v2_agt_80jV_9EA"
       data-name="did-agent"
       data-monitor="true"
-      data-orientation="horizontal"
-      data-position="right"
       data-target-id="did-agent-container">
 </script>
 """
 
 components.html(html_code, height=650)
+
+# import streamlit as st
+# import streamlit.components.v1 as components
+
+# st.title("D-ID AI Agent")
+
+# html_code = """
+# <div id="did-agent-container" style="width: 80%; height: 600px;"></div>
+# <script type="module"
+#       src="https://agent.d-id.com/v2/index.js"
+#       data-mode="fabio"
+#       data-client-key="Z29vZ2xlLW9hdXRoMnwxMTI3NjQ3MzA0NTM3NjA0MTgyMTI6d01EN0x6bFFFMmlZSk9nUHNacXll"
+#       data-agent-id="v2_agt_80jV_9EA"
+#       data-name="did-agent"
+#       data-monitor="true"
+#       data-orientation="horizontal"
+#       data-position="right"
+#       data-target-id="did-agent-container">
+# </script>
+# """
+
+# components.html(html_code, height=650)
  
 
 # import streamlit as st
@@ -114,6 +114,7 @@ components.html(html_code, height=650)
 # surprised - 놀라는 표정
 # happy - 기쁜 표정
 # sad - 슬픈 표정
+
 
 
 
